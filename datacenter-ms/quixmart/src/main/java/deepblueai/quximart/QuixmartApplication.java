bab9b2099a272bb2476d54def51bcffdd100e356
tuomy <tuomy@deepblueai.com>
2020-11-30 21:36:44 +0800

    modify quixmart moudle
    Change-Id: If0a790f776d90271141249f44f4dcb112575498e

--- datacenter-ms/quixmart/src/main/java/deepblueai/quximart/QuixmartApplication.java
+++ datacenter-ms/quixmart/src/main/java/deepblueai/quximart/QuixmartApplication.java
@@ -1,6 +1,5 @@
 package deepblueai.quximart;
 
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
