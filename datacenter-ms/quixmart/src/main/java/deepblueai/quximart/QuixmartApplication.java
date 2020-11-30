67683dca0e59c7236cd24852e0442c92e5a8befb
tuomy <tuomy@deepblueai.com>
2020-11-30 17:43:42 +0800

    add quixmart moudle
    Change-Id: Id3c2c49b04d685ff4640fff6b0791fd962be8ff2

---
+++ datacenter-ms/quixmart/src/main/java/deepblueai/quximart/QuixmartApplication.java
@@ -0,0 +1,18 @@
+package deepblueai.quximart;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@SpringBootApplication
+@EnableEurekaClient
+@EnableFeignClients
+@EnableSwagger2
+public class QuixmartApplication {
+    public static void main(String [] args){
+        SpringApplication.run(QuixmartApplication.class,args);
+    }
+}
