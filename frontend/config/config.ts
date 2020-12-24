31bee7294144a689a6b212ce256f6e0fdfc0460a
daicx <daicx@deepblueai.com>
2020-12-24 19:57:40 +0800

    图片省略模式去掉
    Change-Id: I5fc3422b54c86f879d2a7aba4e66a988512d0e73

--- frontend/config/config.ts
+++ frontend/config/config.ts
@@ -35,7 +35,7 @@ export default defineConfig({
   // @ts-ignore
   title: false,
   ignoreMomentLocale: true,
-  proxy: proxy[REACT_APP_ENV || 'test'],
+  proxy: proxy[REACT_APP_ENV || 'dev'],
   manifest: {
     basePath: '/',
   },
